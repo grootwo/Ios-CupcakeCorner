8abbebde34602f3449173297dc625bd008df80d7
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-22 11:08:44 +0900

    Feat: load image with AsyncImage

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,49 +7,20 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
     var body: some View {
-        List {
-            ForEach(results, id: \.trackId) { result in
-                VStack(alignment: .leading) {
-                    Text(result.trackName)
-                        .font(.headline)
-                    Text(result.collectionName)
-                        .font(.caption)
-                }
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("Error: Failed to load the image")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            await loadData()
-        }
-    }
-    func loadData() async {
-        // 1: read url
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Error: Invalid url")
-            return
-        }
-        do {
-            // 2: fetch data
-            let (data, _) = try await URLSession.shared.data(from: url)
-            // 3: decode data
-            if let decoded = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decoded.results
-            }
-        } catch {
-            print("Error: Invalid data")
-        }
+        .frame(width: 100)
     }
 }
 
