79802c50baa37901aabc82db6a7712d46cbbfa20
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-22 10:30:13 +0900

    Feat: load data

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -22,13 +22,33 @@ struct ContentView: View {
     var body: some View {
         List {
             ForEach(results, id: \.trackId) { result in
-                VStack {
+                VStack(alignment: .leading) {
                     Text(result.trackName)
                         .font(.headline)
                     Text(result.collectionName)
                         .font(.caption)
                 }
             }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    func loadData() async {
+        // 1: read url
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Error: Invalid url")
+            return
+        }
+        do {
+            // 2: fetch data
+            let (data, _) = try await URLSession.shared.data(from: url)
+            // 3: decode data
+            if let decoded = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decoded.results
+            }
+        } catch {
+            print("Error: Invalid data")
         }
     }
 }
