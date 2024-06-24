1ebb4ef4bd330ed59470869cce303564dd41238f
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-24 16:37:27 +0900

    Feat: create simple haptics

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,24 +7,13 @@
 
 import SwiftUI
 
-@Observable
-class User: Codable {
-    enum CodingKeys: String, CodingKey {
-        case _name = "name"
-        case _password = "password"
-    }
-    var name = "New User"
-    var password = 1234
-}
-
 struct ContentView: View {
+    @State private var tapCount = 0
     var body: some View {
-        Button("Decode & Encode", action: encodeUser)
-    }
-    func encodeUser() {
-        let data = try! JSONEncoder().encode(User())
-        let str = String(decoding: data, as: UTF8.self)
-        print(str)
+        Button("tap count: \(tapCount)") {
+            tapCount += 1
+        }
+        .sensoryFeedback(.impact(flexibility: .soft, intensity: 1), trigger: tapCount)
     }
 }
 
