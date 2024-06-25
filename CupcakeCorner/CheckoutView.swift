eaf10750831323f4f3490cd09e995fa6015bbb5a
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-25 11:04:18 +0900

    Feat: set placing order func with network

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
     var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     var body: some View {
         ScrollView {
             VStack {
@@ -21,13 +23,41 @@ struct CheckoutView: View {
                 }
                 Text("Your cost is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
-                Button("Place Order", action: {})
-                    .padding()
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
             }
         }
         .navigationTitle("Check Out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("Okay") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Error: Failed to encode order")
+            return
+        }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity) x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Error: Get \(error.localizedDescription)")
+            return
+        }
     }
 }
 
